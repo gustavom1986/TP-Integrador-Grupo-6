369fb400a9b4a89df02a1d33c5570f451b688d1a
Erick Osuna Saavedra <erick_osuna@hotmail.com>
2023-04-11 13:21:46 -0300

    Agregando Lectura del archivo Equipos.csv

--- TPEntrega1/src/tpentrega1/TPEntrega1.java
+++ TPEntrega1/src/tpentrega1/TPEntrega1.java
@@ -4,6 +4,11 @@
  */
 package tpentrega1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  *
  * @author grupo6
@@ -13,10 +18,22 @@ public class TPEntrega1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         
         
+        // Asignando variable a la ruta de acceso del archivo EQUIPOS.CSV
+        String ubicacionArchivoEquipos = "C:\\Users\\Siscop\\Desktop\\TrabajoPracticoIntegrador\\TP-Integrador-Grupo-6\\equipos.csv";
+        
+        // Trayendo el archivo EQUIPOS.CSV a través del método PATH
+        Path pathEquipos = Paths.get (ubicacionArchivoEquipos);
+        
+        // Leyendo cada línea del archivo EQUIPOS.CSV
+        for (String equipo : Files.readAllLines(pathEquipos)){
+            
+            // Imprimiendo por consola cada línea de EQUIPOS.CSV a modo de prueba.
+            System.out.println(equipo);
+        }
     }
 
 }
